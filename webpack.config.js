acaec7ee9ecf3eb74e22cfad2f8214dd0e22a4f3
Timothy Farrell <tim@thecookiejar.me>
2016-03-04 23:29:26 -0600

    Use the html plugin to build index.html. (and some white space fixes in webpack.config.js)

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var isProduction = process.env.NODE_ENV == 'production';
 
 module.exports = {
-    context: path.join(__dirname, 'src'),
-    entry: {
+		context: path.join(__dirname, 'src'),
+		entry: {
 			main: './main.js',
 			globals: './globals.js'
 		},
-    devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
-    output: {
-      path: path.join(__dirname, 'app'),
-      filename: '[name].bundle.js'
-    },
-    resolve: {
-      extensions: ['', '.js']
-    },
-    module: {
+		devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+		output: {
+			path: path.join(__dirname, 'app'),
+			filename: '[name].bundle.js'
+		},
+		resolve: {
+			extensions: ['', '.js']
+		},
+		module: {
 			preLoaders: [
 				{
 					test: /\.js?$/,
@@ -25,20 +26,24 @@ module.exports = {
 					loader: 'eslint-loader'
 				}
 			],
-      loaders: [
+			loaders: [
 				{
-		      test: /\.js?$/,
-		      exclude: /node_modules/,
-		      loader: 'babel'
-        }
-      ]
-    },
-    plugins: (isProduction ? [
+					test: /\.js?$/,
+					exclude: /node_modules/,
+					loader: 'babel'
+				}
+			]
+		},
+		plugins: (isProduction ? [
 			new webpack.optimize.DedupePlugin(),
-		  new webpack.optimize.OccurenceOrderPlugin(),
-		  new webpack.optimize.AggressiveMergingPlugin(),
-		  new webpack.optimize.MinChunkSizePlugin({minChunkSize: 10000})
+			new webpack.optimize.OccurenceOrderPlugin(),
+			new webpack.optimize.AggressiveMergingPlugin(),
+			new webpack.optimize.MinChunkSizePlugin({minChunkSize: 10000})
 		] : []).concat([
+			new HtmlWebpackPlugin({
+				inject: false,	// some day we can set this to true
+				template: "index.html"
+			}),
 			new webpack.optimize.CommonsChunkPlugin({
 				name: "commons",
 				filename: "commons.js"
