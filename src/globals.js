637a4d956306fffa97ef6cbb46aec9ea4c58ea83
Timothy Farrell <tim@thecookiejar.me>
2016-03-05 08:37:11 -0600

    Use the XRegExp from NPM.

--- src/globals.js
+++ src/globals.js
@@ -1,11 +1,15 @@
 import i18n from './i18n.js';
 import $ from 'jQuery';
 import i18nextJquery from 'jquery-i18next/lib/index';
+import XRegExp from 'xregexp';
 
 // While we are only partially implemented, some tools that exist in in modules
 // must be exposed as globals so that old code can reference them where they
 // are expected. As more libraries are modularized, this file will grow and as
 // application code is modularized this file will shrink.
+
+// XRegExp
+window['XRegExp'] = XRegExp;
 
 // jQuery
 window['$'] = $;
